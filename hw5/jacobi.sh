438f55a85d7aeca03c61fe5e057390be077e2a02
asunwoo98 <45576715+asunwoo98@users.noreply.github.com>
2020-05-06 00:51:03 -0400

    hw5 update

--- hw5/jacobi.sh
+++ hw5/jacobi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=1
 #SBATCH --time=5:00:00
 #SBATCH --mem=4GB
@@ -14,3 +14,4 @@ module load openmpi/gnu/4.0.2
 mpirun -np 64 jacobi2d-mpi 200 1000
 # n = ln * sqrt(p)
 # 1600 = 100 * 16
+# n = 1600
